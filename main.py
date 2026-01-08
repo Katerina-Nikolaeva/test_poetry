bc82a7d62c382e12070d701979b67d1a92de2de6
Ekaterina Nikolaev <maksimova.kate@gmail.com>
2026-01-08 21:24:11 +0300

    Исправление после линтеров

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import get_date, mask_account_card
+from src.processing import filter_by_state
 
 if __name__ == "__main__":
     card_number = "7000792289606361"
@@ -26,3 +27,12 @@ if __name__ == "__main__":
     print(f"-> account data: {date_long}")
     date_short = get_date(date_long)
     print(f"-> masked account data: {date_short}")
+
+    dict_data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                 {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                 {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    state = 'CANCELED'
+    filtered_data = filter_by_state(dict_data,state)
+    print(f"-> filtered data: {filtered_data}")
+
