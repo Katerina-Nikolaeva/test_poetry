ed43a660d9644db6e7eba89a51a074538b22e5c4
Ekaterina Nikolaev <maksimova.kate@gmail.com>
2026-01-08 23:46:34 +0300

    Проверка работы функций

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import get_date, mask_account_card
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 
 if __name__ == "__main__":
     card_number = "7000792289606361"
@@ -36,3 +36,11 @@ if __name__ == "__main__":
     filtered_data = filter_by_state(dict_data,state)
     print(f"-> filtered data: {filtered_data}")
 
+
+    data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                 {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                 {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    order ='descending'
+    sorted_data = sort_by_date(data,order)
+    print(f"-> sorted data: {sorted_data}")
