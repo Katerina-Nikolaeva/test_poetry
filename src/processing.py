c01992e9434120635777724669f91be90044b8a6
Ekaterina Nikolaev <maksimova.kate@gmail.com>
2026-01-09 01:02:21 +0300

    Создание файла с описанием проекта

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 
-def filter_by_state (dict_data, state: 'EXECUTED')-> list:
+def filter_by_state (dict_data, state: str = 'EXECUTED') -> list:
     """
     Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state = EXECUTED
 
@@ -27,25 +27,28 @@ def filter_by_state (dict_data, state: 'EXECUTED')-> list:
     # filtered_data = [item for item in dict_data if item.get('state') == state ]
     # return filtered_data
 
-def sort_by_date (data, order='descending')-> list:
+def sort_by_date (data, order: str ='descending')-> list:
     """
-        Функция сортирует список словарей по дате (ключ 'date').
+    Функция сортирует список словарей по дате (ключ 'date').
 
-        Parameters:
-        - data (list of dict): Список словарей для сортировки.
-        - order (str, optional): Порядок сортировки ('ascending' или 'descending').
-        Примеры:
-          - [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}]
+    Parameters:
+      - data (list of dict): Список словарей для сортировки.
+      - order (str, optional): Порядок сортировки ('ascending' или 'descending').
+    Примеры:
+      - [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}]
 
-        Returns:
-          - sorted_data (list of dict): Отсортированный список словарей.
-        Пример:
-          - [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    Returns:
+      - sorted_data (list of dict): Отсортированный список словарей.
+
+    Пример:
+      - [
+          {'id': 41428829,  'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}]
-        """
+          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        ]
+    """
     sorted_data = sorted(
         data,
         key=lambda item: datetime.fromisoformat(item.get('date')),
